36f99af8ced767cf064dc05e0ed8ecf30fd83d1a
felix-mohrmann <mohrmann.felix@gmail.com>
2021-07-15 17:32:03 +0200

    Add Array Classes

---
+++ src/main/java/felix/fische/app/Array.java
@@ -0,0 +1,13 @@
+package felix.fische.app;
+
+public class Array {
+
+    public static String[] fillArray(int amountOfStudents) {
+        String[] students = new String[amountOfStudents];
+
+        for (int i = 0; i < students.length; i++){
+            students[i] = "Students " + (i+1);
+        }
+        return students;
+    }
+}
---
+++ src/test/java/felix/fische/app/ArrayTest.java
@@ -0,0 +1,17 @@
+package felix.fische.app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ArrayTest {
+    @Test
+    public void arrayFillsCorrect(){
+        //given
+        int arrayLength = 5;
+        String[] expected = {"Students 1", "Students 2", "Students 3", "Students 4", "Students 5"};
+        //when
+        String[] actual = Array.fillArray(arrayLength);
+        //then
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
